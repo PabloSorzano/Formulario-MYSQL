dfb043d61859eaeae33403172b249722115f4761
xrockbellx <30833363+xrockbellx@users.noreply.github.com>
2017-08-22 22:21:27 -0500

    Add files via upload

---
+++ formulario.sql
@@ -0,0 +1,6 @@
+drop database formulario ;
+create database formulario;
+use formulario;
+create table Datos(Nombre varchar(50),Apat varchar(50),Amat varchar(50), Edad varchar(5), Escuela varchar(50));
+Describe datos;
+select*from datos;
